2f7ccb6f84a0ac4e4ac232b952c48a5480b913a0
Marvin Taube <marvin.taube@pigeonport.de>
2023-09-13 11:43:19 +0200

    Bugfix and removed unnecessary user check

--- registration/registrationConfirmed.php
+++ registration/registrationConfirmed.php
@@ -5,45 +5,21 @@ namespace easyNewsletter;
 $email = $_GET["email"];
 $token = $_GET["token"];
 
-if (databaseConnector::instance()->getSettingFromDB('subscriberMode') == 'user'){
-	$user = get_user_by("email", $email);
 
+$query = new \WP_Query(array("post_type" => "en_subscribers", "posts_per_page" => "-1"));
 
-	if (get_user_meta($user->ID, 'en_token', true) == $token){
-		echo get_the_content("", false, databaseConnector::instance()->getSettingFromDB("confirmationSuccessPageID"));
-	    update_user_meta($user->ID, "en_doubleOptIn", "confirmed");
-	    update_user_meta($user->ID, "en_status", "active");
-        mailManager::instance()->sendWelcomeMail($email, $token);
-    }
-    else{
-	    farnLog::log("Could not confirm Registration: Invalid token for given Email: ".$email);
-	    echo get_the_content("", false, databaseConnector::instance()->getSettingFromDB("confirmationDeniedPageID"));
-    }
+while ($query->have_posts()){
+	$query->the_post();
 
-}else{
-	$query = new \WP_Query(array("post_type" => "en_subscribers", "posts_per_page" => "-1"));
-
-	while ($query->have_posts()){
-		$query->the_post();
-
-		if (get_post_meta( get_the_ID(),"en_eMailAddress", true) == $email){
-			if (get_post_meta( get_the_ID(),"en_token", true) == $token){
-				echo get_the_content("", false, databaseConnector::instance()->getSettingFromDB("confirmationSuccessPageID"));
-				update_post_meta(get_the_ID(), "en_doubleOptIn", "confirmed");
-				update_post_meta(get_the_ID(), "en_status", "active");
-				mailManager::instance()->sendWelcomeMail($email, $token);
-			}
-			else{
-				farnLog::log("Could not confirm Registration: Invalid token for given Email: ".$email);
-				echo get_the_content("", false, databaseConnector::instance()->getSettingFromDB("confirmationDeniedPageID"));
-			}
-		}
-		else{
-			farnLog::log("Could not confirm Registration: Invalid Email: ".$email);
-			echo get_the_content("", false, databaseConnector::instance()->getSettingFromDB("confirmationDeniedPageID"));
+	if (get_post_meta( get_the_ID(),"en_eMailAddress", true) == $email){
+		if (get_post_meta( get_the_ID(),"en_token", true) == $token){
+			echo get_the_content("", false, databaseConnector::instance()->getSettingFromDB("confirmationSuccessPageID"));
+			update_post_meta(get_the_ID(), "en_doubleOptIn", "confirmed");
+			update_post_meta(get_the_ID(), "en_status", "active");
+			mailManager::instance()->sendWelcomeMail($email, $token);
+			return;
 		}
 	}
 }
-
-//
-?>
+farnLog::log("Could not confirm Registration: Invalid Email: ".$email);
+echo get_the_content("", false, databaseConnector::instance()->getSettingFromDB("confirmationDeniedPageID"));
